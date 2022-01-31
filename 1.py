223a77dcf48d17b1e1fa31639d5736a6e5acc003
Khalid SHARAFAT <97115184+kha1lid1@users.noreply.github.com>
2022-01-31 20:00:12 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,11 +3,11 @@ from time import sleep
 import colorsys
 
 client = opc.Client('localhost:7890')
-leds = [(25,0,0)]*360
+leds = [(0,0,0)]*360
 
-print ('whats your name')
-myname = input ()
-print ('HI,' + myname)
+print ('whats your name')                                             #Prints whats your name. Asks for a Nmae.
+myname = input ()                                                     #input your name.
+print ('HI,' + myname)                                                #prints Hi + the inpued name.
 
 
 HI = [29,30,33,34,36,37,38,39,40,41,89,90,93,94,96,97,98,
@@ -15,18 +15,18 @@ HI = [29,30,33,34,36,37,38,39,40,41,89,90,93,94,96,97,98,
       211,212,213,214,218,219,269,270,273,274,276,277,278,
       279,280,281,329,330,333,334,336,337,338,339,340,341]
 for i in HI:
-    leds [i] = (255,0,0)
+    leds [i] = (255,0,0)                                             #tuns on the specifyed the LEDs.
     client.put_pixels(leds)
     sleep(0.03)
 
+
+
 HI = [29,30,33,34,36,37,38,39,40,41,89,90,93,94,96,97,98,
       99,100,101,149,150,151,152,153,154,158,159,209,210,
       211,212,213,214,218,219,269,270,273,274,276,277,278,
       279,280,281,329,330,333,334,336,337,338,339,340,341]
 for i in HI:
-    leds [i] = (0,0,0)
+    leds [i] = (0,0,0)                                              #tuens off the specifyed LEDs.
     client.put_pixels(leds)
     sleep(0.003)
-
-
 
